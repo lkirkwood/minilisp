53b58cdbe3c9af2a19193fa18fa6bd72d119f9f9
Linus Kirkwood <linuskirkwood@gmail.com>
2025-09-09 23:40:29 +1000

    Add tokeniser

--- main.rkt
+++ main.rkt
@@ -1,19 +1,58 @@
 #lang racket
 
-(define (parse program-string)
-  (let loop ([program (list)]
-             [stack (list 'S '$)]
-             [chars (string->list program-string)])
-    (let ([stack-top (car stack)]
-          [cur-char (car chars)])
-      (if (eq? cur-char stack-top)
-          (loop program (cdr stack) (cdr chars))
+(define (stack-push stack . elements)
+  (append elements stack))
 
-          (match (list cur-char stack-top)
-            [(list #\( 'S) (loop program (append (list #\( 'paren-expr #\)) stack) (cdr chars))]
-            [(list #\space _) (loop program stack)]
-            [(list #\( 'expr) (loop program (append (list #\( 'paren-expr #\)) stack) (cdr chars))]
-            [(list)])))))
+;; TERMINALS:
+;; (, ), +, −, =, ?, ×, λ, ≜
+(define TERMINALS (set #\( #\) #\+ #\− #\= #\? #\× #\λ #\≜))
+
+(define (number-token chars)
+  (define-values (total _)
+    (for/fold ([total 0]
+               [place-value 1])
+              ([char chars])
+      (define num (- (char->integer char) 48))
+      (values (+ total (* place-value num)) (* 10 place-value))))
+  total)
+
+(define (identifier-token chars)
+  (string->symbol (list->string (reverse chars))))
+
+(define (tokenise program-string)
+  (let loop ([tokens (list)]
+             [token-buf (list)]
+             [buffered-type null]
+             [chars (string->list program-string)])
+    (if (null? chars)
+        (reverse tokens)
+
+        (match (car chars)
+          [(? (curry set-member? TERMINALS) char)
+           (match buffered-type
+             [(? null?) (loop (cons char tokens) (list) null (cdr chars))]
+             ['number
+              (loop (cons char (cons (number-token token-buf) tokens)) (list) null (cdr chars))]
+             ['identifier
+              (loop (cons char (cons (identifier-token token-buf) tokens)) (list) null (cdr chars))])]
+
+          [(? char-numeric? char)
+           (if (or (eq? buffered-type null) (eq? buffered-type 'number))
+               (loop tokens (cons char token-buf) 'number (cdr chars))
+               (raise (format "Found number in the middle of a ~a token" buffered-type)))]
+
+          [(? char-alphabetic? char)
+           (if (or (eq? buffered-type null) (eq? buffered-type 'identifier))
+               (loop tokens (cons char token-buf) 'identifier (cdr chars))
+               (raise (format "Found alphanumeric character in the middle of a ~a token"
+                              buffered-type)))]
+
+          [#\space
+           (match buffered-type
+             [(? null?) (loop tokens (list) null (cdr chars))]
+             ['number (loop (cons (number-token token-buf) tokens) (list) null (cdr chars))]
+             ['identifier
+              (loop (cons (identifier-token token-buf) tokens) (list) null (cdr chars))])]))))
 
 (module+ test
   ;; Any code in this `test` submodule runs when this file is run using DrRacket
@@ -21,7 +60,10 @@
   ;; required by another module.
   (require rackunit)
 
-  (check-equal? (+ 2 2) 4))
+  (check-equal? (tokenise "(+ 123 456)") (list #\( #\+ 123 456 #\)))
+  (check-equal? (tokenise "(+ (× 1 42) (− 42 0))")
+                (list #\( #\+ #\( #\× 1 42 #\) #\( #\− 42 0 #\) #\)))
+  (check-equal? (tokenise "(≜ myident (× 42 100))") (list #\( #\≜ 'myident #\( #\× 42 100 #\) #\))))
 
 (module+ main
   ;; (Optional) main submodule. Put code here if you need it to be executed when
