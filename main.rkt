4065181e7b89ef04253a8b8775c3aa3f15b0705b
Linus Kirkwood <linuskirkwood@gmail.com>
2025-09-11 16:36:33 +1000

    Add basic cli

--- main.rkt
+++ main.rkt
@@ -233,14 +233,7 @@
   (check-equal? (run "(¬ (= 5 3))") #t))
 
 (module+ main
-  ;; (Optional) main submodule. Put code here if you need it to be executed when
-  ;; this file is run using DrRacket or the `racket` executable.  The code here
-  ;; does not run when this file is required by another module. Documentation:
-  ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
   (require racket/cmdline)
-  (define who (box "world"))
-  (command-line #:program "my-program"
-                #:once-each [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-                #:args ()
-                (printf "hello ~a~n" (unbox who))))
+  (command-line #:program "minilisp"
+                #:args (progfile-path)
+                (run (port->string (open-input-file progfile-path)))))
