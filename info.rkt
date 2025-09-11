cc3c1800a72b7dcf794a1a4deeb93c0f9bc46ec9
Linus Kirkwood <linuskirkwood@gmail.com>
2025-09-11 16:46:34 +1000

    Update package info

--- info.rkt
+++ info.rkt
@@ -1,9 +1,8 @@
 #lang info
 (define collection "minilisp")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/minilisp.scrbl" ())))
-(define pkg-desc "Description Here")
+(define build-deps '("rackunit-lib"))
+(define pkg-desc "An aggressively LL(1) parseable LISP.")
 (define version "0.0")
 (define pkg-authors '("Linus Kirkwood"))
-(define license '(MIT))
+(define license '(LGPL-3.0))
